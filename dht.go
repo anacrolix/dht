03bc7abe8781cbbcc7ee05af8ce5f2b7c95514bf
Matt Joiner <anacrolix@gmail.com>
2018-02-19 20:27:34 +1100

    Add some more global bootstrap addrs

--- dht.go
+++ dht.go
@@ -85,7 +85,10 @@ func GlobalBootstrapAddrs() (addrs []Addr, err error) {
 		"router.utorrent.com:6881",
 		"router.bittorrent.com:6881",
 		"dht.transmissionbt.com:6881",
-		"dht.aelitis.com:6881", // Vuze
+		"dht.aelitis.com:6881",     // Vuze
+		"router.silotis.us:6881",   // IPv6
+		"dht.libtorrent.org:25401", // @arvidn's
+
 	} {
 		host, port, err := net.SplitHostPort(s)
 		if err != nil {

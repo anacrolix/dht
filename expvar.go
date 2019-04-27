b51b1061529d728173525128ca77b38eb839db42
Matt Joiner <anacrolix@gmail.com>
2019-04-27 15:21:11 +1000

    Remove expvar_dht Prometheus export
    Use github.com/anacrolix/missinggo/prometheus/NewExpvarCollector instead.

--- expvar.go
+++ expvar.go
@@ -2,16 +2,8 @@ package dht
 
 import (
 	"expvar"
-
-	"github.com/prometheus/client_golang/prometheus"
 )
 
 var (
 	expvars = expvar.NewMap("dht")
 )
-
-func init() {
-	prometheus.MustRegister(prometheus.NewExpvarCollector(map[string]*prometheus.Desc{
-		"dht": prometheus.NewDesc("expvar_dht", "", []string{"key"}, nil),
-	}))
-}

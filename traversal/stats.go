c363f114b09fcd6478ef0b54566eaa4cd268c769
Matt Joiner <anacrolix@gmail.com>
2021-10-14 21:08:41 +1100

    traversal.Stats: Unimplement Stringer
    %#v uses hexadecimal for the field values. Instead, I think you should use %+v and pass the Stats value. I don't know any way to make Go do this neatly, my attempt to this in Stats.String resulted in a recursive call.

--- traversal/stats.go
+++ traversal/stats.go
@@ -1,16 +1,8 @@
 package traversal
 
-import (
-	"fmt"
-)
-
 type Stats struct {
 	// Count of (probably) distinct addresses we've sent traversal queries to. Accessed with atomic.
 	NumAddrsTried uint32
 	// Number of responses we received to queries related to this traversal. Accessed with atomic.
 	NumResponses uint32
-}
-
-func (me Stats) String() string {
-	return fmt.Sprintf("%#v", me)
 }

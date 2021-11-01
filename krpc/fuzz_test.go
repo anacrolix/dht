41a76066160f2278ddfaf52da019bcf779f27170
Matt Joiner <anacrolix@gmail.com>
2021-11-01 11:58:46 +1100

    Some changes to krpc.Msg fuzzing

--- krpc/fuzz_test.go
+++ krpc/fuzz_test.go
@@ -10,18 +10,22 @@ import (
 )
 
 func Fuzz(f *testing.F) {
-	//for _, ret := range random_encode_tests {
-	//	f.Add([]byte(ret.expected))
-	//}
+	f.Add([]byte("d1:rd2:id20:\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x01e1:t1:t1:y1:re"))
 	f.Fuzz(func(t *testing.T, b []byte) {
 		c := qt.New(t)
 		var m Msg
 		err := bencode.Unmarshal(b, &m)
-		if err != nil || m.T == "" || m.Q == "" {
+		if err != nil || m.T == "" || m.Y == "" {
 			t.Skip()
 		}
+		if m.R != nil {
+			if m.R.ID == [20]byte{} {
+				c.Skip()
+			}
+		}
 		b0, err := bencode.Marshal(m)
+		c.Logf("%q -> %q", b, b0)
 		c.Assert(err, qt.IsNil)
-		c.Assert(b0, qt.DeepEquals, b)
+		c.Assert(string(b0), qt.Equals, string(b))
 	})
 }
